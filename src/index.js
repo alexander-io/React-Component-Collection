3aac9bffde05279c50b36cc23dc9cdaf68363410
alexander-io <abharris@pugetsound.edu>
2017-07-15 18:29:46 -0700

    ++learn more about React-Components

--- src/index.js
+++ src/index.js
@@ -3,18 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css'
 
 
-
 let root = document.getElementById('root')
-let branch = document.getElementById('branch');
-
-let s = <h1>ah-vak-oh-dew</h1>
-let c = <h1>ah-vak-oh-dewww</h1>
-
-//
-// let getTime = () => {
-//   return getTime
-//
-
 
 function tick() {
   const curr_time = <h1>{new Date().toLocaleTimeString()}</h1>
@@ -24,12 +13,68 @@ function tick() {
 
 setInterval(tick, 1000)
 
+/*
+ * components let you split hte ui into independent, reusable pieces, and think about each piece in isolation
+ * conceptually, components are javascript functions. they accept arbitrary inputs (called 'props') and return react elements describing what should appear on the screen
+ */
 
+// the simplest way to define a component is to write a javascript function
+// function Welcome(props) {
+//   return <h1>hello, {props.name}</h1>
+//   /*
+//    * this is a valid react component because it accepts a single 'props' object argument with data and returns a react element. we call such components functional because they are literally javascript functions
+//    *
+//    *
+//   */
+// }
 
+//similarly, you can use an es6 class to define a component
+// the aforementioned way of defining a react component is equivalent to using the 'class' style of definition
+// class Welcome extends React.Component {
+//   render () {
+//     return <h1>hello, {this.props.name}</h1>
+//   }
+// }
 
+// RENDERING A COMPONENT, https://facebook.github.io/react/docs/components-and-props.html
+// previously we encountered react elements that represent DOM tags :
+// const dom_tag_element = <div />;
 
+// however, elements can also represent user-defined components
 
+// const user_defined_component = <Welcome name="Grace" />;
 
+// XXX when react sees an element representing a user-defined component, it passes jsx attributes to this component as a single object. we call this object props
+
+// note, by convention components identifiers always start with a capital letter
+
+// for example, the following renders  'hello, grace' on the page :
+// function Welcome_User(props) {
+//   return (
+//     <div>
+//       <h1>
+//         hello, {props.name}
+//       </h1>
+//       <h2>
+//         user : {props.user}
+//       </h2>
+//       <h2>
+//         location : {props.location}
+//       </h2>
+//     </div>
+//   )
+// }
+//
+//
+//
+// const welcome_elem = <Welcome_User name="grace" user="gracily" location="seattle"/>;
+//
+// ReactDOM.render(welcome_elem, root)
+
+// let branch = document.getElementById('branch');
+// let s = <h1>ah-vak-oh-dew</h1>
+// let c = <h1>ah-vak-oh-dewww</h1>
+// TODO Functional and Class Components
 
 //
 // class Square extends React.Component {
